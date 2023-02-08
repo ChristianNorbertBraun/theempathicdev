3db9cb20b22d1edcce35dc5ea0e790d5c719e325
Christian Braun <braun.christian.bamberg@gmail.com>
2023-02-08 12:41:30 +0100

    Build first cards

---
+++ src/lib/Card.svelte
@@ -0,0 +1,8 @@
+<script>
+    let clazz = "";
+    export { clazz as class };
+</script>
+
+<div class="bg-red-500 h-96 rounded-xl p-4 flex flex-col gap-4 {clazz}">
+    <slot/>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Card from '../lib/Card.svelte';
+</script>
+
+<div class="grid grid-cols-1 w-screen p-4 gap-4">
+	<Card class="gradient text-gray-800">
+		<h1 class="text-4xl font-semibold">I'm Christian iOS Freelancer with 6 years of experience</h1>
+		<p>
+			I care a lot about human friendly interface design and excellent code quality. Don’t take my
+			word for it, check out my portfolio.
+		</p>
+		<div class="mt-auto">
+			<a
+				class="bg-pink-500 text-white px-4 py-2 rounded-full text-lg"
+				href="mailto:christian.braun@theempathicdev.de">Contact me</a
+			>
+		</div>
+	</Card>
+	<Card class="bg-[url('/img/avatar.jpeg')] bg-cover bg-center">
+	</Card>
+</div>
+
+<style>
+	:global(.gradient) {
+		background: rgb(34, 193, 195);
+		background: linear-gradient(57deg, rgba(34, 193, 195, 1) 0%, rgba(253, 187, 45, 1) 100%);
+	}
+</style>
