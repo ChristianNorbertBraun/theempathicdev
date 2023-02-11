88c9acfcb642f8d89e6a87414b0be0cba5fdf77f
Christian Braun <braun.christian.bamberg@gmail.com>
2023-02-11 16:55:03 +0100

    Make link for button adjustable

--- src/lib/ArrowButton.svelte
+++ src/lib/ArrowButton.svelte
@@ -1,4 +1,11 @@
-<a class="mt-auto ml-auto" href="https://anchor.fm/indexoutofbounds">
+<script language="typescript">
+	 /**
+	 * @type {string}
+	 */
+	  export let link;
+</script>
+
+<a class="mt-auto ml-auto" href="{link}">
 	<div
 		class="backdrop-blur-md bg-white/30 rounded-full w-14 h-14 text-center items-center justify-center flex"
 	>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,11 +22,11 @@ import Card from '../lib/Card.svelte';
 	<Card class="bg-black md:col-span-5 text-white">
 		<h2 class="text-2xl font-semibold">My Podcast</h2>
 		<img class="max-h-[65%] object-contain justify-self-center place-content-center" src="img/ioob.jpg" alt="The logo of the Index out of Bounds podcast"/>
-		<ArrowButton/>
+		<ArrowButton link="https://anchor.fm/indexoutofbounds"/>
 	</Card>
 	<Card class="bg-emerald-400 md:col-span-5 text-white bg-[url('/img/portfolio.png')] bg-contain bg-origin-padding bg-[bottom_-5rem_right_1.5rem] md:bg-[bottom_-3rem_right_1.5rem] bg-no-repeat">
 		<h2 class="text-2xl font-semibold">My Portfolio</h2>
 		<p>Talk is easy, action is what counts. View a list of selected projects I have been involved in.</p>
-		<ArrowButton/>
+		<ArrowButton link="/portfolio"/>
 	</Card>
 </div>
