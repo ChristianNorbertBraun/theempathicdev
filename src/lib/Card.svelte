f06f66add5a8cdbfaa5344d5e650153267954f4f
Christian Braun <braun.christian.bamberg@gmail.com>
2023-02-13 16:52:26 +0100

    Add contact buttons

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -3,6 +3,6 @@
     export { clazz as class };
 </script>
 
-<div class="bg-gray-400 h-96 md:h-[35rem] rounded-xl p-8 flex flex-col gap-4 hover:scale-[101%] transition overflow-scroll ease-linear {clazz}">
+<div class="bg-gray-400 md:h-[35rem] rounded-xl p-8 flex flex-col gap-4 hover:scale-[101%] transition ease-linear {clazz}">
     <slot/>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 	import Card from '../lib/Card.svelte';
 </script>
 
-<div class="max-w-6xl m-auto w-screen p-4">
+<div class="max-w-6xl m-auto w-screen p-4 bg-white md:mt-4 md:rounded-xl">
 	<div class="pb-4 flex gap-1 items-center">
 		<div class="h-3 w-3 bg-red-600 rounded-full" />
 		<div class="h-3 w-3 bg-yellow-400 rounded-full" />
@@ -17,15 +17,29 @@
 				I care a lot about human friendly interface design and excellent code quality. Don’t take my
 				word for it, check out my portfolio.
 			</p>
-			<div class="mt-auto">
+			<div class="mt-auto flex items-center">
 				<a
-					class="bg-pink-500 text-white px-4 py-2 rounded-full text-lg"
+					class="bg-emerald-400 text-gray-100  px-4 py-2 rounded-full text-lg hover:bg-gradient-to-tr hover:from-[#7B02FC] hover:to-[#F402D3]"
 					href="mailto:christian.braun@theempathicdev.de">Contact me</a
 				>
+				<div class="ml-4 flex gap-2 h-full">
+					<a
+						class="bg-gray-400 text-gray-100 flex justify-center items-center h-full aspect-square  rounded-full text-lg hover:bg-gradient-to-tr hover:from-[#7B02FC] hover:to-[#F402D3]"
+						href="https://twitter.com/NorbiBraun">
+						<img class="w-5" src="/img/twitter.svg" alt="Twitter logo"/>
+					</a>
+					
+					<a
+						class="bg-gray-400 text-gray-100 flex justify-center items-center h-full aspect-square rounded-full text-lg hover:bg-gradient-to-tr hover:from-[#7B02FC] hover:to-[#F402D3]"
+						href="https://github.com/ChristianNorbertBraun">
+						<img class="w-5" src="/img/github.svg" alt="Github logo"/>
+						</a
+					>
+				</div>
 			</div>
 		</Card>
-		<Card class="bg-[url('/img/avatar.jpeg')] bg-cover bg-center md:col-span-4" />
-		<Card class="bg-black md:col-span-5 text-white">
+		<Card class="bg-[url('/img/avatar.jpeg')] bg-cover bg-center md:col-span-4 h-96" />
+		<Card class="bg-black md:col-span-5 text-white h-96">
 			<h2 class="text-2xl font-semibold">My Podcast</h2>
 			<img
 				class="max-h-[65%] object-contain justify-self-center place-content-center"
@@ -35,7 +49,7 @@
 			<ArrowButton link="https://anchor.fm/indexoutofbounds" />
 		</Card>
 		<Card
-			class="bg-emerald-400 md:col-span-5 text-white bg-[url('/img/portfolio.png')] bg-contain bg-origin-padding bg-[bottom_-5rem_right_1.5rem] md:bg-[bottom_-3rem_right_1.5rem] bg-no-repeat"
+			class="bg-[#95B39D] md:col-span-5 h-96 text-white bg-[url('/img/portfolio.png')] bg-contain bg-origin-padding bg-[bottom_-5rem_right_1.5rem] md:bg-[bottom_-3rem_right_1.5rem] bg-no-repeat"
 		>
 			<h2 class="text-2xl font-semibold">My Portfolio</h2>
 			<p>
@@ -45,4 +59,11 @@
 			<ArrowButton link="/pdf/Portfolio_Christian_Braun_tac.pdf" />
 		</Card>
 	</div>
+
+	<footer class="mt-4">
+		<hr class="w-full" />
+		<div class="flex mt-4">
+			<a class="hover:bg-black hover:text-white" href="/imprint">Imprint</a>
+		</div>
+	</footer>
 </div>
