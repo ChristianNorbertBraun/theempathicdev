146a9cfb90f11e3fe7d07bf795c7714d0f0eb4c9
Christian Braun <braun.christian.bamberg@gmail.com>
2023-02-14 16:14:21 +0100

    Move header and footer into layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,19 @@
 	import '../app.postcss';
 </script>
 
-<slot />
+<div class="max-w-6xl m-auto w-screen p-4 bg-white md:my-4 md:rounded-xl">
+	<div class="pb-4 flex gap-1 items-center">
+		<div class="h-3 w-3 bg-red-600 rounded-full" />
+		<div class="h-3 w-3 bg-yellow-400 rounded-full" />
+		<div class="h-3 w-3 bg-green-500 rounded-full" />
+		<span class="ml-auto">The empathic dev</span>
+	</div>
+	<slot />
+
+	<footer class="mt-4">
+		<hr class="w-full" />
+		<div class="flex mt-4">
+			<a class="hover:bg-black hover:text-white" href="/imprint">Imprint</a>
+		</div>
+	</footer>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,6 @@
 	import Card from '../lib/Card.svelte';
 </script>
 
-<div class="max-w-6xl m-auto w-screen p-4 bg-white md:mt-4 md:rounded-xl">
-	<div class="pb-4 flex gap-1 items-center">
-		<div class="h-3 w-3 bg-red-600 rounded-full" />
-		<div class="h-3 w-3 bg-yellow-400 rounded-full" />
-		<div class="h-3 w-3 bg-green-500 rounded-full" />
-		<span class="ml-auto">The empathic dev</span>
-	</div>
 	<div class="grid grid-cols-1 gap-4 md:grid-cols-10">
 		<Card class="bg-gradient-to-tr from-slate-300 to-blue-200 text-gray-800 md:col-span-6">
 			<h1 class="text-4xl font-bold">I'm Christian, iOS Freelancer with 6 years of experience</h1>
@@ -59,11 +52,3 @@
 			<ArrowButton link="/pdf/Portfolio_Christian_Braun_tac.pdf" />
 		</Card>
 	</div>
-
-	<footer class="mt-4">
-		<hr class="w-full" />
-		<div class="flex mt-4">
-			<a class="hover:bg-black hover:text-white" href="/imprint">Imprint</a>
-		</div>
-	</footer>
-</div>
