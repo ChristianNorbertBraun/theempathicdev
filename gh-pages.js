97bf5347d7ebd78643e40578ae6ec945a3139135
Christian Braun <braun.christian.bamberg@gmail.com>
2023-02-14 15:16:30 +0100

    Update to use ssh repo url

--- gh-pages.js
+++ gh-pages.js
@@ -4,7 +4,7 @@ publish(
  'build', // path to public directory
  {
   branch: 'gh-pages',
-  repo: 'https://github.com/ChristianNorbertBraun/theempathicdev.git', // Update to point to your repository
+  repo: 'git@github.com:ChristianNorbertBraun/theempathicdev.git', // Update to point to your repository
   user: {
    name: 'Christian Braun', // update to use your name
    email: 'christian.braun@theempathicdev.de' // Update to use your email
