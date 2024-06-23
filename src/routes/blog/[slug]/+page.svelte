09829de5ba3de0b1a585b9471a6558881a885efe
Christian Braun <braun.christian.bamberg@gmail.com>
2024-06-23 16:54:39 +0200

    Ensure code elements are horizontally scrollable
    Seems like flexbox destorys somewhow the overflow detection on the <code> element.

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -7,12 +7,12 @@
 	$: component = data.component as unknown as C;
 </script>
 
-<div class="flex flex-col items-center">
+<div class="md:flex md:items-center md:justify-center">
 	<div>
 		<h1 class="text-4xl font-bold pb-4">
 			{data.frontmatter.title}
 		</h1>
-		<article class="prose prose-a:underline prose-a:decoration-emerald-400 prose-a:decoration-4 prose-code:bg-slate-200 prose-code:rounded prose-code:font-normal prose-code:p-px">
+		<article class="prose prose-a:underline prose-a:decoration-emerald-400 prose-a:decoration-4 prose-code:bg-slate-200 prose-code:rounded prose-code:font-normal prose-code:p-px prose-code:overflow-y-scroll prose-code:max-w-1 pre:overflow-y-scroll">
 			<svelte:component this={component} />
 		</article>
 	</div>
