c879de1884f9f4d77f12231d5ba5c46de47a81a4
Christian Braun <braun.christian.bamberg@gmail.com>
2024-07-16 12:11:35 +0200

    Add date to blog post

--- src/routes/(pages)/blog/[slug]/+page.svelte
+++ src/routes/(pages)/blog/[slug]/+page.svelte
@@ -5,6 +5,16 @@
 	export let data: PageData;
 	type C = $$Generic<typeof SvelteComponentTyped<any, any, any>>;
 	$: component = data.component as unknown as C;
+
+	function dateString(date) {
+		let formattedDate = new Date(date).toLocaleDateString('en-US', {
+			weekday: 'long', // "Monday"
+			year: 'numeric', // "2023"
+			month: 'long', // "January"
+			day: 'numeric' // "1"
+		});
+		return formattedDate;
+	}
 </script>
 
 <div class="md:flex md:items-center md:justify-center">
@@ -12,6 +22,7 @@
 		<h1 class="text-4xl font-bold pb-4">
 			{data.frontmatter.title}
 		</h1>
+		<p class="text-sm text-slate-500">{dateString(data.frontmatter.date)}</p>
 		<article class="prose prose-a:underline prose-a:decoration-emerald-400 prose-a:decoration-4 prose-code:bg-slate-200 prose-code:rounded prose-code:font-normal prose-code:p-px prose-code:overflow-y-scroll prose-code:max-w-1 pre:overflow-y-scroll">
 			<svelte:component this={component} />
 		</article>
