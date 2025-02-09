0274069043e3cbf38c36f394d427d2dbdda81c12
Christian Braun <christian.braun@theempathicdev.de>
2025-02-09 23:44:29 +0100

    Updates

---
+++ _app/immutable/chunks/6-236fc0e3.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/(pages)/imprint/_page.svelte-d230fd93.js";export{t as component};
--- _app/immutable/chunks/6-c6221e79.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/(pages)/imprint/_page.svelte-aa42610e.js";export{t as component};
